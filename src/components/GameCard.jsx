491ed88c5ab690331ad415f5a31338fe35cb57fa
fbuitron <franciscobuitron21@gmail.com>
2026-04-03 18:30:26 -0700

    Style changes + mobile responsiveness added, time improvements

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -15,6 +15,21 @@ const GameCard = ({ game, onDetailsClick }) => {
     });
   };
 
+  const formatGameStatus = (status) => {
+    if (!status) return { period: "", formattedTime: "" };
+
+    if (!status.includes(' ')) {
+      return { period: status, formattedTime: "" };
+    }
+
+    const [period, time] = status.split(' ');
+    const formattedTime = time.startsWith(':') ? time.substring(1) : time;
+
+    return { period, formattedTime };
+  };
+
+  const { period, formattedTime } = formatGameStatus(game.time);
+
   return (
     <Card className={game.status === '1st Qtr' || game.status === '2nd Qtr' || game.status === '3rd Qtr' || game.status === '4th Qtr' ? "game-card live-game mt-3" : "game-card mt-3"}>
       <Card.Body>
@@ -32,22 +47,22 @@ const GameCard = ({ game, onDetailsClick }) => {
         <Card.Title>{game.visitor_team.name} vs. {game.home_team.name}</Card.Title>
 
         <Row className="mt-4 g-0">
-          <Col sm={4} className="align-center">
-            <Row className="g-1">
-              <Col sm={3}>
-                <VisitorLogo size={45} />
+          <Col xl={3} lg={2} md={3} className="align-center">
+            <Row>
+              <Col md={12} xs={4} className="visitor-logo">
+                <VisitorLogo size={75} />
               </Col>
 
-              <Col className="text-left">
-                <h5>{game.visitor_team.name}</h5>
+              <Col xs={12}>
+                <h5 className="team-name">{game.visitor_team.name}</h5>
               </Col>
             </Row>
           </Col>
 
-          <Col sm={4}>
+          <Col xl={6} lg={8} md={6} className="score-row">
             {game.status === '1st Qtr' || game.status === '2nd Qtr' || game.status === '3rd Qtr' || game.status === '4th Qtr' || game.status === 'Final' ?
               <Row className="g-0">
-                <Col sm={4}>
+                <Col xs={4}>
                   <div className="scorecard">
                     <p className={game.visitor_team_score > game.home_team_score ? "higher-score" : "lower-score"}>
                       {game.visitor_team.abbreviation}
@@ -56,23 +71,51 @@ const GameCard = ({ game, onDetailsClick }) => {
                       {game.visitor_team_score}
                     </p>
                     {game.status !== 'Final' &&
-                      <p>TO: {game.visitor_timeouts_remaining}</p>
+                      <div>
+                        {[...Array(7)].map((timeouts, index) => {
+                          const isActive = index < game.visitor_timeouts_remaining;
+                          
+                          return (
+                            <div
+                              key={index}
+                              className={`timeout-bar ${isActive ? 'active' : 'spent'}`}
+                            />
+                          );
+                        })}
+                      </div>
                     }
                   </div>
                 </Col>
 
-                <Col sm={4} className="align-center">
-                  <h5>{game.time}</h5>
+                <Col xs={4} className="align-center">
+                  <h5>
+                    <span>{period}</span>
+                    <br />
+                  {game.status !== 'Final' &&
+                    <span className={`fw-bold ${formattedTime.includes('.') ? 'text-danger' : ''}`}>{formattedTime}</span>
+                  }
+                  </h5>
                 </Col>
 
-                <Col sm={4}>
+                <Col xs={4}>
                   <div className="scorecard">
                     <p className={game.visitor_team_score < game.home_team_score ? "higher-score" : "lower-score"}>
                       {game.home_team.abbreviation}
                     </p>
                     <p className={game.visitor_team_score < game.home_team_score ? "higher-score" : "lower-score"}>{game.home_team_score}</p>
                     {game.status !== 'Final' &&
-                      <p>TO: {game.home_timeouts_remaining}</p>
+                      <div>
+                        {[...Array(7)].map((timeouts, index) => {
+                          const isActive = index < game.home_timeouts_remaining;
+                          
+                          return (
+                            <div
+                              key={index}
+                              className={`timeout-bar ${isActive ? 'active' : 'spent'}`}
+                            />
+                          );
+                        })}
+                      </div>
                     }
                   </div>
                 </Col>
@@ -84,14 +127,14 @@ const GameCard = ({ game, onDetailsClick }) => {
             }
           </Col>
 
-          <Col sm={4} className="align-center">
-            <Row className="g-1">
-              <Col className="text-right">
-                <h5>{game.home_team.name}</h5>
+          <Col xl={3} lg={2} md={3} className="align-center">
+            <Row>
+              <Col md={{ span: 12, offset: 0}} xs={{ span: 4, offset: 8}} className="home-logo">
+                <HomeLogo size={75} />
               </Col>
 
-              <Col sm={3}>
-                <HomeLogo size={45} />
+              <Col xs={12}>
+                <h5 className="team-name">{game.home_team.name}</h5>
               </Col>
             </Row>
           </Col>
